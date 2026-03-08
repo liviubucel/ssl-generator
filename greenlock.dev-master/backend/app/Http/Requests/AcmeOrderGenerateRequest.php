db150dc9bb04c4cb24c97aeb4723b7284434e0e8
Liviu Bucel <109367578+liviubucel@users.noreply.github.com>
2026-03-08 19:32:43 +0000

    Add files via upload

---
+++ greenlock.dev-master/backend/app/Http/Requests/AcmeOrderGenerateRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AcmeOrderGenerateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'domains' => 'required|array',
+            'domains.*' => 'string',
+            'email' => 'required|email',
+        ];
+    }
+
+    /**
+     * The function prepares input data for validation by converting a comma-separated string into an
+     * array.
+     */
+    protected function prepareForValidation(): void
+    {
+        if (isset($this->domains) && !empty($this->domains)) {
+            $this->merge([
+                'domains' => explode(',', $this->domains),
+            ]);
+        }
+    }
+}
